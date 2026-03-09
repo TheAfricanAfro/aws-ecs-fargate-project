abf90b6013904d65d211d72fd25fcff12da486f5
Ahmed Abdo <mr.abdo199@gmail.com>
2026-03-08 22:11:02 -0400

    iam.tf finished

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,28 +1,28 @@
-#Creating exec role to prep env
+#Creating exec role to prep
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "project-ecs-execution-roletest_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2008-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
         Sid    = ""
+        Effect = "Allow"
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = "ecs-tasks.amazonaws.com"
         }
-      },
+        Action = "sts:AssumeRole"
+      }
     ]
   })
 
   tags = {
-    tag-key = "tag-value"
+    Name = "project-ecs-execution-roletest_role"
   }
 }
 
 #Attaches policy (AWS Managed) to the role. Gives ECR pull permissions. 
-resource "aws_iam_role_policy_attachment" "test-attach" {
+resource "aws_iam_role_policy_attachment" "attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
