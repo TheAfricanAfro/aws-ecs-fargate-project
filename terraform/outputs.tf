1a1e71e3c53f90a159f6c315537cd15718e43436
Ahmed Abdo <mr.abdo199@gmail.com>
2026-04-11 20:47:47 -0400

    updating main.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,9 +3,3 @@ output "project_alb_dns_name" {
   description = "DNS name of the Application Load Balancer"
   value       = aws_lb.project_application_lb.dns_name
 }
-
-# Allows us to upload image to repo
-output "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.project_ecr_repo.repository_url
-}
